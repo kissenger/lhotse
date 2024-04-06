fcf9fc619992ad29c6d21ee2a040a90425ebdc03
kissenger <ivyterrace@hotmail.co.uk>
2024-04-06 19:40:09 +0100

    reinstate environment files

---
+++ environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  PROTOCOL: 'https',
+  IMGIX_DOMAIN: 'beta-snorkelology.imgix.net',
+  BACKEND_URL: 'localhost:3000/api',
+  INSTA_TESTER_TOKEN: 'IGQWRPX1FFWm4tWUp2R09SZAVhEWWloN0RwUnllYmU5VExSS2VXR0lYY25YVXp4a0duUTE4eWFiS3hqWnl0RTAxWk1uajNadXN2eFBLZAFV4ZAUJOYVpIc295bkRFWHR4WDZAUdG9oR1B5amRiZAWNZASEpaUkpUSUV4cFkZD',
+};
