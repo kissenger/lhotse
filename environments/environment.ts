2f5ec0e51b36e5b50d06ed11ccc03310d134a45d
kissenger <ivyterrace@hotmail.co.uk>
2024-04-06 19:45:56 +0100

    nothing interesting

--- environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  PROTOCOL: 'https',
-  IMGIX_DOMAIN: 'snorkelology.imgix.net',
-  BACKEND_URL: 'snorkelology.co.uk/api',
-  INSTA_TESTER_TOKEN: 'IGQWRPX1FFWm4tWUp2R09SZAVhEWWloN0RwUnllYmU5VExSS2VXR0lYY25YVXp4a0duUTE4eWFiS3hqWnl0RTAxWk1uajNadXN2eFBLZAFV4ZAUJOYVpIc295bkRFWHR4WDZAUdG9oR1B5amRiZAWNZASEpaUkpUSUV4cFkZD',
-};
