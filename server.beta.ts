651a2144f5b5ffce450071c1647ae7e386d6d1fd
kissenger <ivyterrace@hotmail.co.uk>
2024-07-13 20:04:40 +0100

    test

--- server.beta.ts
+++ server.beta.ts
@@ -5,62 +5,29 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 // import { MongoClient } from 'mongodb'
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
 
 // **** API setup - added gst
-import 'dotenv/config'
+// import 'dotenv/config'
 // import { MongoClient, ServerApiVersion } from 'mongodb';
 // import ContactsModel from './schema/contact';
-// import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 
 const pwd = process.env['MONGODB_PASSWORD'];
 const db = process.env['MONGODB_DBNAME'];
-const cs = `mongodb+srv://root:Gudd5QfVXUMI2uzS@cluster0-5h6di.gcp.mongodb.net/snorkelology?retryWrites=true&w=majority&appName=Cluster0`;
-// console.log(cs);
+const cs = `mongodb+srv://root:Gudd5QfVXUMI2uzS@cluster0.5h6di.gcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-// console.log('boobies')
-// const client = new MongoClient(cs, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
+const client = new MongoClient(cs, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+console.log(client)
 
-main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect(cs);
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
-
-// try {
-//   const client = new MongoClient(cs, {
-//     // serverApi: ServerApiVersion.v1
-//   });
-//   client.connect().then( (conn) => {
-//     console.log('MongoDB connected')
-//   });
-// } catch(e) {
-//   // console.log(e)
-// }
-
-// let conn; 
-
-// try {
-
-// } catch(e) {
-//   console.error(e);
-//   console.log('MongoDB Error')
-// }
-// mongoose.connection
-//   .on('error', console.error.bind(console, 'connection error:'))
-//   .on('close', () => console.log('MongoDB disconnected'))
-//   .once('open', () => console.log('MongoDB connected') );
-// **** End of API setup
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -74,12 +41,11 @@ export function app(): express.Express {
   server.set('views', browserDistFolder);
 
   
-  // *** API Endpoints
     
-  server.use(express.json());
-  server.get('/api/ping/', (req, res) => {
-    res.status(201).json({hello: 'world'});
-  })
+//   server.use(express.json());
+//   server.get('/api/ping/', (req, res) => {
+//     res.status(201).json({hello: 'world'});
+//   })
 
   // server.post('/api/store-email', async (req, res) => {
   //   try {
