444ce9e5437047e005bfd920105d907625a3d40f
kissenger <ivyterrace@hotmail.co.uk>
2024-10-25 13:07:56 +0100

    banner ad working

--- src/app/shared/components/banner-ad/banner-ad.component.ts
+++ src/app/shared/components/banner-ad/banner-ad.component.ts
@@ -10,13 +10,16 @@ import { Component} from '@angular/core';
       <img class=""
         ngSrc = "ads/snorkelling-britain-preorder-now-banner.jpg"
         alt   = "Snorkelling Britain: 100 Marine Adventures book cover"
-        width = "728"
-        height = "90"
+        fill
       /> 
     </div> 
   `,
-  styles: ''
+  styles: ` div{overflow:hidden;position:relative;height:90px;width:100%;border:1px red solid;} 
+            div>img{width:100%;height:100%;object-fit:cover;object-position:center;}
+            @media (max-width: 575px) {div{height: 70px;}}
+          `
 })
+
 export class BannerAdComponent{
 
   constructor(
