0dcc60b21eb67f69aa7c0cabbd08b628cbb3f29a
kissenger <ivyterrace@hotmail.co.uk>
2024-10-31 13:28:42 +0000

    more structured data testing

--- src/app/pages/blog/shower/post-shower.component.ts
+++ src/app/pages/blog/shower/post-shower.component.ts
@@ -117,7 +117,7 @@ export class PostShowerComponent implements OnDestroy, OnInit, AfterViewInit {
               "name": "${q.question}",
               "acceptedAnswer": {
                 "@type": "Answer",
-                "text": "${q.answer}"}}`
+                "text": "${q.answer.split('\n')[0]}"}}`
             }).join(",");
 
             this._seo.addStructuredData(`
