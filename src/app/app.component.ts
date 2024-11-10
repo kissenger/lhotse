9e2f74b97e10ab87ede869059359de6d89bbd582
kissenger <ivyterrace@hotmail.co.uk>
2024-11-10 14:26:30 +0000

    with route tracing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { HeaderComponent } from '@shared/components/header/header.component';
 import { FooterComponent } from '@shared/components/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule, DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
+// import { Router } from 'express';
 
 
 @Component({
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, CommonModule],
+  providers: [Router],
+  imports: [HeaderComponent, FooterComponent , CommonModule, RouterOutlet],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -17,10 +20,12 @@ export class AppComponent {
   public showHeaderFooter: boolean;
 
   constructor(
-    @Inject(DOCUMENT) private _document: Document
+    @Inject(DOCUMENT) private _document: Document,
+    private _router: Router
   ) {
     let _window = _document.defaultView;
     this.showHeaderFooter = _window?.location.href.match('blogeditor') === null;
+    this._router.events.subscribe(console.log)
   }
 
 
